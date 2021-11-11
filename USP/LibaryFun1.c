0c20268770e2124906f4f4be927cb8817538d379
Ankit Raj Biswal <ankitrudra2001@gmail.com>
2021-11-11 08:45:47 +0530

    Update LibaryFun1.c

--- USP/LibaryFun1.c
+++ USP/LibaryFun1.c
@@ -1,9 +1,4 @@
-/*
-Prgrammer: Rituraj Gupta
-Date completed: 28/10/2021
-Instructor Dr. T.N.Pandey
-Registration Number: 1941012660
-*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #define pi 3.14159
