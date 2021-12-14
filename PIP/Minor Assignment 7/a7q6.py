ebd9050c9692bdf8a5ba02c4512fab521928fe28
Ankit Raj Biswal <ankitrudra2001@gmail.com>
2021-12-14 09:46:44 +0530

    Create a7q6.py

---
+++ PIP/Minor Assignment 7/a7q6.py
@@ -0,0 +1,11 @@
+def funclist(n):
+  lst=[]
+  lst2=[0,0,0,0,0]
+  for i in range(1,n+1):
+    for j in range(1,6):
+      lst2[j-1]=i*j
+    lst.append(lst2[:])
+  return lst
+def main():
+  n=int(input("Enter a Number : "))
+  print(funclist(n))
