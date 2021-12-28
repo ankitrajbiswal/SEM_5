76d590c13e20b1c2e678d3681e9b00045f8e7877
Ankit Raj Biswal <ankitrudra2001@gmail.com>
2021-12-28 08:43:36 +0530

    Update a8q10.py

--- PIP/Minor Assignment 8/a8q10.py
+++ PIP/Minor Assignment 8/a8q10.py
@@ -5,3 +5,8 @@ def findStrictlyIncreasingNum(start, out, n):
     for i in range(start, 10):
         str1 = out + str(i)
         findStrictlyIncreasingNum(i + 1,str1, n - 1)
+     
+#Owned By
+Ankit Raj Biswal
+1941012238
+ankitrudra2001@gmail.com
